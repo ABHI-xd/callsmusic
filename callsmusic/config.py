1dc62c96333d3052e95e40cc549f5edc79bdf234
ABHI-xd <94308307+ABHI-xd@users.noreply.github.com>
2021-12-18 11:23:44 +0530

    Update config.py

--- callsmusic/config.py
+++ callsmusic/config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 SESSION_NAME = getenv('SESSION_NAME', 'session')
 BOT_TOKEN = getenv('BOT_TOKEN')
-API_ID = int(getenv('API_ID', ''))
+API_ID = int(getenv('API_ID', '6'))
 API_HASH = getenv('API_HASH')
 DURATION_LIMIT = int(getenv('DURATION_LIMIT', '7'))
 COMMAND_PREFIXES = list(getenv('COMMAND_PREFIXES', '/ !').split())
